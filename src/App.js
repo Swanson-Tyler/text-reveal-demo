188481d31d22dbba661dbf3d42ff3cfab06b112d
Tyler  Swanson <tyler.swanson@firstborn.com>
2020-03-27 23:47:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {CharacterReveal} from "react-text-reveal";
+
 
 function App() {
+  const [animate, setAnimate] = useState(false);
+
+useEffect(() => {
+  setTimeout(() => {
+    setAnimate(true)
+;  }, 1000);
+})
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +19,18 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CharacterReveal
+            animateOpacity={true}
+            canPlay={animate}
+            characterOffsetDelay={25} // ms
+            characterWordSpacing={"10px"}
+            copy={["Make.","Something.","Move."]}
+            direction={"bottom"}
+            duration={1275} // ms
+            ease={"cubic-bezier(0,0.4,0.4,1)"}
+            offset={"45px"}
+            multilineOffsetDelay={200} // ms
+          />
       </header>
     </div>
   );
